63217de8aee1d10f1da0879f34188398426dc214
vorpax <64530538+vorpax@users.noreply.github.com>
2025-11-01 00:42:15 +0100

    refactor: clean up create.go by removing unused functions and variables

--- cmd/create.go
+++ cmd/create.go
@@ -1,20 +1,15 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-*/
+// Package cmd pve-eztemplate create /*
 package cmd
 
 import (
 	"fmt"
-	"net/url"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
+
+	"pve-eztemplate/internal"
 
 	"github.com/spf13/cobra"
 )
 
-var unparsed_url_arg string
+var unparsedUrlArg string
 
 // createCmd represents the create command
 var createCmd = &cobra.Command{
@@ -29,13 +24,14 @@ to quickly create a Cobra application.`,
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("create called")
-		createTemplate(args[0])
+		//vm.CreateTemplate(args[0])
+		internal.TestCode()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(createCmd)
-	createCmd.Flags().StringVarP(&unparsed_url_arg, "source", "s", "", "Source directory to read from")
+	createCmd.Flags().StringVarP(&unparsedUrlArg, "source", "s", "", "Source directory to read from")
 
 	// Here you will define your flags and configuration settings.
 
@@ -46,41 +42,4 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-func createTemplate(unparsed_url string) {
-	parsed_url, err := url.Parse(unparsed_url)
-
-	if err != nil {
-
-	}
-
-	download_template(parsed_url)
-}
-
-func download_template(parsed_url *url.URL) (download_path string, err string) {
-	filename := parsed_url.Path[strings.LastIndex(parsed_url.Path, "/")+1:]
-
-	// Utiliser le répertoire courant de travail
-	wd, wd_err := os.Getwd()
-
-	if wd_err != nil {
-
-		fmt.Println("Error getting working directory:", err)
-		return
-	}
-
-	download_path = filepath.Join(wd, "data", filename)
-
-	fmt.Printf("Filename: %s\n", filename)
-	fmt.Printf("Download path: %s\n", download_path)
-
-	command := exec.Command("curl", "-o", download_path, parsed_url.String())
-	fmt.Println("Command:", command)
-
-	if err := command.Run(); err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	return download_path, err
 }
