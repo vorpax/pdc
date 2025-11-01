8183ae8d137eec5cde20385882bc8b8d422654de
vorpax <64530538+vorpax@users.noreply.github.com>
2025-11-01 12:29:07 +0100

    feat: implement destroy command for Proxmox VM templates with configuration handling

---
+++ cmd/destroy.go
@@ -0,0 +1,52 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"pve-eztemplate/internal"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// destroyCmd represents the destroy command
+var destroyCmd = &cobra.Command{
+	Use:   "destroy",
+	Short: "Destroy an existing Proxmox VM template",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		destroyTemplate(
+			viper.GetString("vm.id"),
+			viper.GetBool("verbose"),
+		)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(destroyCmd)
+	destroyCmd.Flags().String("vm-id", "", "The unique ID for the new VM/template")
+	viper.BindPFlag("vm.id", destroyCmd.Flags().Lookup("vm-id"))
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// destroyCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// destroyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func destroyTemplate(vmId string, verbose bool) error {
+	internal.DestroyVm(vmId, verbose)
+
+	return nil
+}
